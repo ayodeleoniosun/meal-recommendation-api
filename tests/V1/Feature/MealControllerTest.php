874f9b8654a51a0e8e336865eb3701cc9ce48304
Dev-AY <heywire4u@gmail.com>
2021-08-06 12:35:29 +0100

    test: single meal details

--- tests/V1/Feature/MealControllerTest.php
+++ tests/V1/Feature/MealControllerTest.php
@@ -2,7 +2,14 @@
 
 namespace Tests\V1\Feature;
 
+use App\Api\V1\Models\Allergy;
+use App\Api\V1\Models\MainItem;
+use App\Api\V1\Models\Meal;
+use App\Api\V1\Models\MealToAllergy;
+use App\Api\V1\Models\MealToSideItem;
+use App\Api\V1\Models\SideItem;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\DB;
 use Tests\V1\TestCase;
 
 class MealControllerTest extends TestCase
@@ -21,7 +28,62 @@ class MealControllerTest extends TestCase
         $this->assertEquals($response->getData()->status, 'success');
         $response->assertJsonStructure([
             'status',
-            'meals'
+            'meals' => [
+                '*' => [
+                    'id', 'name', 'created_at', 'updated_at', 'active_status',
+                    'main_item' => [
+                        'id', 'name', 'created_at', 'updated_at', 'active_status'
+                    ],
+                    'side_items' => [
+                        '*' => ['id', 'name', 'created_at', 'updated_at', 'active_status']
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    public function testShowSingleMeal()
+    {
+        $count = 1;
+        $allergies = factory(Allergy::class, 3)->create();
+        $meal = factory(Meal::class, 1)->create();
+        $meal = Meal::find($meal[0]->id);
+        $sideItems = factory(SideItem::class, 5)->create();
+
+        DB::transaction(function () use ($meal, $allergies, $sideItems, $count) {
+            factory(MainItem::class, 1)->create([
+                'meal_id' => $meal->id
+            ]);
+
+            foreach ($sideItems as $side_item) {
+                factory(MealToSideItem::class, $count)->create([
+                    'meal_id' => $meal->id,
+                    'side_item_id' => $side_item->id
+                ]);
+            }
+
+            foreach ($allergies as $allergy) {
+                factory(MealToAllergy::class, $count)->create([
+                    'meal_id' => $meal->id,
+                    'allergy_id' => $allergy->id
+                ]);
+            }
+        });
+
+        $response = $this->json('GET', $this->route("/meals/".$meal->id));
+        $response->assertStatus(200);
+        $this->assertEquals($response->getData()->status, 'success');
+        $response->assertJsonStructure([
+            'status',
+            'meal' => [
+                'id', 'name', 'created_at', 'updated_at', 'active_status',
+                'main_item' => [
+                    'id', 'name', 'created_at', 'updated_at', 'active_status'
+                ],
+                'side_items' => [
+                    '*' => ['id', 'name', 'created_at', 'updated_at', 'active_status']
+                ]
+            ]
         ]);
     }
 }
