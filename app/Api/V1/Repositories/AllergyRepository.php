2a051040787c80dca03839e1ff4d5d539ad4d672
Dev-AY <heywire4u@gmail.com>
2021-08-06 23:09:55 +0100

    refactor: pick allergy

--- app/Api/V1/Repositories/AllergyRepository.php
+++ app/Api/V1/Repositories/AllergyRepository.php
@@ -33,27 +33,38 @@ class AllergyRepository implements AllergyInterface
         $user = User::find($data->auth_user->id);
         
         $allergies = Allergy::whereIn('id', $data->allergies)->active()->pluck('id')->toArray();
+        
+        if (count($allergies) == 0) {
+            throw new CustomApiErrorResponseHandler("Allergies do not exist");
+        }
+
         $userAllergies = collect($user->allergies->pluck('id')->toArray());
         $allAllergies = count($allergies) > count($userAllergies) ? collect($allergies)->diff($userAllergies) :
             collect($userAllergies)->diff($allergies);
 
         if (count($allAllergies) == 0) {
-            throw new CustomApiErrorResponseHandler("No allergy picked");
+            throw new CustomApiErrorResponseHandler("No new allergy picked");
         }
 
-        foreach ($allAllergies as $allergy) {
-            UserToAllergy::create([
-                'user_id' => $data->auth_user->id,
-                'allergy_id' => $allergy
-            ]);
-        }
+        $userToAllergies = UserToAllergy::where('user_id', $data->auth_user->id)
+            ->whereIn('allergy_id', $allAllergies)->pluck('allergy_id')->toArray();
         
-        $allergies = $user->load('allergies');
+        $newUserAllergies = [];
+
+        foreach ($allAllergies as $allergy) {
+            if (!in_array($allergy, $userToAllergies)) {
+                $newUserAllergies[] = [
+                    'user_id' => $data->auth_user->id,
+                    'allergy_id' => $allergy
+                ];
+            }
+        }
+
+        UserToAllergy::insert($newUserAllergies);
         
         return [
             'status' => 'success',
-            'allergies' => $allergies->allergies,
-            'message' => count($allAllergies).' allergies successfully picked'
+            'message' => count($newUserAllergies).' allergies successfully picked'
         ];
     }
 }
